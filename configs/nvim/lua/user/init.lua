a4b017df8c857c3b415dae048b4ee8506488d47e
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-04-28 18:11:37 -0700

    Make nvim autoreload file when modified externally

--- configs/nvim/lua/user/init.lua
+++ configs/nvim/lua/user/init.lua
@@ -76,5 +76,10 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    -- Auto-reload files when modified externally
+    vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+      command = "if mode() != 'c' | checktime | endif",
+      pattern = { "*" },
+    })
   end,
 }
