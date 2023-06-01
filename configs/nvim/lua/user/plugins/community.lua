d0536c2b31dec4b648c411eb71049ae285419a99
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-05-31 21:29:16 -0700

    Fix breaking changes to dracula-nvim theme

--- configs/nvim/lua/user/plugins/community.lua
+++ configs/nvim/lua/user/plugins/community.lua
@@ -4,7 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.dracula" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.comment.mini-comment" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.markdown-and-latex.vimtex" },
