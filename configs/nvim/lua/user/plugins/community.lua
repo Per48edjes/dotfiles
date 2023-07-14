d77df05db60b6c25c283746b4293c45aeb432731
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-07-14 13:13:26 -0700

    Add C/C++ AstroNvim community pack

--- configs/nvim/lua/user/plugins/community.lua
+++ configs/nvim/lua/user/plugins/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.haskell" },
 }
