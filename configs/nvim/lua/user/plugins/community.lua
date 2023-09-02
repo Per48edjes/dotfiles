c8486bcd459bfd7261617370a4d4cdbc2e72b6d1
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-09-02 14:42:17 -0700

    Remove `haskell-tools.nvim` hack

--- configs/nvim/lua/user/plugins/community.lua
+++ configs/nvim/lua/user/plugins/community.lua
@@ -1,6 +1,8 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  -- For local AstroCommunity development, use the following instead:
+  -- { dir = "~/code/astrocommunity" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
@@ -15,22 +17,4 @@ return {
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.haskell" },
-  -- HACK: Overwriting AstroCommunity Haskell pack
-  {
-    "mrcjkb/haskell-tools.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim", -- Optional
-    },
-    branch = "2.x.x",                  -- Recommended
-    init = function()                  -- Optional, see Advanced configuration
-      vim.g.haskell_tools = {
-        tools = {},
-        hls = {
-          on_attach = function(client, bufnr, ht) end,
-        },
-      }
-    end,
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-  },
 }
