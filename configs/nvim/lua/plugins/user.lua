530487d82af78ad54663df37df3404263b930c43
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2024-05-26 04:20:52 -0400

    Config VimTex for Neovim

--- configs/nvim/lua/plugins/user.lua
+++ configs/nvim/lua/plugins/user.lua
@@ -102,10 +102,22 @@ return {
   },
   {
     "lervag/vimtex",
-    lazy = false, -- we don't want to lazy load VimTeX
-    -- tag = "v2.15", -- uncomment to pin to a specific release
-    init = function()
-      -- VimTeX configuration goes here
+    ft = "tex",
+    config = function()
+      vim.g.tex_flavor = "tex"
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_quickfix_mode = 1
+      vim.g.vimtex_fold_enabled = 1
+      vim.g.tex_conceal = "abdmg"
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          "--shell-escape",
+          "-verbose",
+          "-file-line-error",
+          "-synctex=1",
+          "-interaction=nonstopmode",
+        },
+      }
     end,
   },
 }
