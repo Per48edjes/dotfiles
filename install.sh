f2a6c905a4668d23bc0f9ea97c2c908332213f2b
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2024-07-08 20:47:46 -0700

    Fix installer borked by formatter

--- install.sh
+++ install.sh
@@ -1,143 +1,141 @@
 #!/usr/bin/env bash
 
-## INSTALLATION bash -c "$(curl -#fL
-#raw.githubusercontent.com/Per48edjes/dotfiles/master/install.sh)"
+## INSTALLATION
+# bash -c "$(curl -#fL raw.githubusercontent.com/Per48edjes/dotfiles/master/install.sh)"
 
-LOG="${HOME}/Library/Logs/dotfiles.log" GITHUB_USER="Per48edjes"
-GITHUB_REPO="dotfiles" DIR="${HOME}/code/${GITHUB_REPO}"
+LOG="${HOME}/Library/Logs/dotfiles.log"
+GITHUB_USER="Per48edjes"
+GITHUB_REPO="dotfiles"
+DIR="${HOME}/code/${GITHUB_REPO}"
 
-_process() { echo "$(date) PROCESSING:  $@" >> $LOG printf "$(tput setaf 6)
-    %s...$(tput sgr0)\n" "$@" }
+_process() {
+	echo "$(date) PROCESSING: $*" >>"$LOG"
+	printf "$(tput setaf 6) %s...$(tput sgr0)\n" "$@"
+}
 
-    _success() { local message=$1 printf "%s✓ Success:%s\n" "$(tput setaf 2)"
-        "$(tput sgr0) $message" }
+_success() {
+	local message=$1
+	printf "%s✓ Success:%s\n" "$(tput setaf 2)" "$(tput sgr0) $message"
+}
 
-        download_dotfiles() { _process "→ Downloading repository to /tmp
-            directory" curl -#fLo /tmp/${GITHUB_REPO}.tar.gz
-            "https://github.com/${GITHUB_USER}/${GITHUB_REPO}/tarball/master"
+download_dotfiles() {
+	mkdir -p "${DIR}"
 
-            _process "→ Extracting files to ${DIR}" tar -zxf
-            /tmp/${GITHUB_REPO}.tar.gz --strip-components 1 -C "${DIR}"
+	_process "→ Downloading repository to /tmp directory"
+	curl -#fLo /tmp/${GITHUB_REPO}.tar.gz "https://github.com/${GITHUB_USER}/${GITHUB_REPO}/tarball/master"
 
-            _process "→ Removing tarball from /tmp directory" rm -rf
-            /tmp/${GITHUB_REPO}.tar.gz
+	_process "→ Extracting files to ${DIR}"
+	tar -zxf /tmp/${GITHUB_REPO}.tar.gz --strip-components 1 -C "${DIR}"
 
-            [[ $? ]] && _success "${DIR} created, repository downloaded and
-            extracted"
+	_process "→ Removing tarball from /tmp directory"
+	rm -rf /tmp/${GITHUB_REPO}.tar.gz
 
-    # Change to the dotfiles directory
-    cd "${DIR}" }
+	[[ $? ]] && _success "${DIR} created, repository downloaded and extracted"
 
-    link_dotfiles() {
-        # Symlink files to the HOME directory.
-        if [[ -f "${DIR}/opt/dotfiles" ]]; then _process "→ Symlinking dotfiles
-            in /configs"
+	# Change to the dotfiles directory
+	cd "${DIR}" || exit
+}
 
-        # Set variable for list of files
-        files="${DIR}/opt/dotfiles"
+link_dotfiles() {
+	# Symlink files to the HOME directory.
+	if [[ -f "${DIR}/opt/dotfiles" ]]; then
+		_process "→ Symlinking dotfiles in /configs"
 
-        # Store IFS separator within a temp variable
-        OIFS=$IFS
-        # Set the separator to a carriage return & a new line break read in
-        # passed-in file and store as an array
-        IFS=$'\r\n' links=($(cat "${files}"))
+		# Set variable for list of files
+		files="${DIR}/opt/dotfiles"
 
-        # Loop through array of files
-        for index in ${!links[*]} do for link in ${links[$index]} do _process
-            "→ Linking ${links[$index]}"
-            # set IFS back to space to split string on
-            IFS=$' '
-            # create an array of line items
-            file=(${links[$index]})
-            # Create symbolic link
-            ln -fs "${DIR}/${file[0]}" "${HOME}/${file[1]}" done
-            # set separater back to carriage return & new line break
-            IFS=$'\r\n' done
+		# Loop through each line in the file
+		while IFS= read -r line; do
+			# Split the line into source and destination
+			IFS=' ' read -r src dst <<<"$line"
+			_process "→ Linking ${dst}"
+			# Create symbolic link
+			ln -fs "${DIR}/${src}" "${HOME}/${dst}"
+		done <"$files"
 
-        # Reset IFS back
-        IFS=$OIFS
+		[[ $? ]] && _success "All files have been copied"
+	fi
+}
 
-        [[ $? ]] && _success "All files have been copied" fi }
+install_homebrew() {
+	_process "→ Installing Homebrew"
+	xcode-select --install
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	_process "→ Running brew doctor"
+	brew doctor
 
-        install_homebrew() { _process "→ Installing Homebrew" xcode-select
-            --install /bin/bash -c "$(curl -fsSL
-            https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	[[ $? ]] && _success "Installed Homebrew"
+}
 
-            _process "→ Running brew doctor" brew doctor
+install_formulae() {
+	if ! type -P 'brew' &>/dev/null; then
+		echo "Homebrew not found"
+		exit 1
+	else
+		_process "→ Installing Homebrew packages"
 
-            [[ $? ]] \ && _success "Installed Homebrew" }
+		# Update and upgrade all packages
+		_process "→ Updating and upgrading Homebrew packages"
+		brew update
+		brew upgrade
+		brew bundle install --file "${DIR}/configs/Brewfile"
+		brew cleanup
+	fi
 
-            install_formulae() { if ! type -P 'brew' &> /dev/null; then _error
-                "Homebrew not found" else _process "→ Installing Homebrew
-                packages"
+	[[ $? ]] && _success "All Homebrew packages installed and updated"
+}
 
-    # Update and upgrade all packages
-    _process "→ Updating and upgrading Homebrew packages" brew update brew
-    upgrade brew bundle install --file "${DIR}/configs/Brewfile" brew cleanup
+install_custom() {
+	_process "→ Finishing install with custom code"
 
-    [[ $? ]] && _success "All Homebrew packages installed and updated" fi }
+	# GHCup
+	curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
-    install_custom() { _process "→ Finishing install with custom code"
+	# Install custom plugins (and themes) for Oh My Zsh
+	git clone https://github.com/Aloxaf/fzf-tab "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/fzf-tab
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+	git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-z
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+	git clone https://github.com/tmux-plugins/tpm "${HOME}"/.tmux/plugins/tpm && chmod 755 "${HOME}"/.tmux
 
-  # Install custom plugins (and themes) for Oh My Zsh
-  git clone https://github.com/Aloxaf/fzf-tab
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab git clone
-  https://github.com/zsh-users/zsh-autosuggestions
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions git clone
-  https://github.com/zsh-users/zsh-syntax-highlighting.git
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting git clone
-  https://github.com/agkozak/zsh-z
-  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm && chmod 755 ${HOME}/.tmux
+	# Set a custom wallpaper image
+	osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"${DIR}/desktop/default_background.jpg"'"'
 
-  # Install AstroNvim
-  git clone --depth 1 https://github.com/AstroNvim/AstroNvim ${HOME}/.config/nvim
-
-  # Set a custom wallpaper image
-  osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"${DIR}/desktop/default_background.jpg"'"'
-
-  [[ $? ]] && _success "All custom code installed"
+	[[ $? ]] && _success "All custom code installed"
 }
 
 manual_preadjustments() {
-  # Make directories
-  _process "→ Creating directory at ${HOME}/.vim"
-  mkdir -p ${HOME}/.vim
+	# Oh-My-Zsh
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  _process "→ Creating directory at ${HOME}/.config/nvim/lua"
-  mkdir -p ${HOME}/.config/nvim/lua
+	# Set terminal italics and true color
+	tic -x "${DIR}/configs/xterm-256color-italic.terminfo"
 
-  # Oh-My-Zsh 
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  # GHCup
-  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-
-  [[ $? ]] && _success "Manual adjustments made successfully"
+	[[ $? ]] && _success "Manual adjustments made successfully"
 }
 
 update_macos_settings() {
-    # Make .macos executable 
-    _process "→ Setting file permissions"
-    chmod u+x "${DIR}/bin/.macos"
+	# Make .macos executable
+	_process "→ Setting file permissions"
+	chmod u+x "${DIR}/bin/.macos"
 
-    # Execute .macos executable 
-    _process "→ Updating macOS settings"
-    ${DIR}/bin/.macos
+	# Execute .macos executable
+	_process "→ Updating macOS settings"
+	"${DIR}"/bin/.macos
 
-    [[ $? ]] && _success "All configured macOS settings have been updated"
+	[[ $? ]] && _success "All configured macOS settings have been updated"
 }
 
 install() {
-  download_dotfiles
-  manual_preadjustments
-  install_homebrew
-  install_formulae
-  link_dotfiles
-  install_custom
-  # TODO: Fix macOS settings setup script
-  update_macos_settings
+	download_dotfiles
+	manual_preadjustments
+	install_homebrew
+	install_formulae
+	link_dotfiles
+	install_custom
+	# TODO: Fix macOS settings setup script
+	update_macos_settings
 }
 
 install
