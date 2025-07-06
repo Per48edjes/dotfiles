c7a5b0a08a9778f650aa0e39b8ddd7029c6a34c6
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2025-07-05 20:21:51 -0700

    Turn off treesitter for LaTex (favor VimTex)

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -10,5 +10,10 @@ return {
       "vim",
       -- add more arguments for adding more treesitter parsers
     })
+    -- Turn off treesitter highlight for LaTeX per vimtex docs
+    opts.ignore_install = { "latex" }
+    if not opts.highlight then opts.highlight = {} end
+    if not opts.highlight.disable then opts.highlight.disable = {} end
+    table.insert(opts.highlight.disable, "latex")
   end,
 }
